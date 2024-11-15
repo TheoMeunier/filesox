cf12ce120b015b72201ed40e4bb1dba1290bf035
tmeunier <theo.meunier41@gmail.com>
2024-11-15 13:10:21 +0100

    fix: update password with 8 character(s)

--- src/main/kotlin/fr/tmeunier/domaine/seeder/UserSeeder.kt
+++ src/main/kotlin/fr/tmeunier/domaine/seeder/UserSeeder.kt
@@ -7,7 +7,7 @@ import fr.tmeunier.domaine.repositories.UsersPermissionsRepository
 object UserSeeder {
     suspend fun seed() {
         if (UserRepository.findByEmail("admin@filesox.fr") == null) {
-            val userId = UserRepository.create("admin", "admin@filesox.fr", "admin", null)
+            val userId = UserRepository.create(null, "admin", "admin@filesox.fr", "filesox", null)
             val adminRole = PermissionRepository.findName("Administration")
 
             UsersPermissionsRepository.sync(userId, listOf(adminRole.id))
