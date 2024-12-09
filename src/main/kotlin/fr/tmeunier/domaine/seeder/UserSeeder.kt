e881a86f63cce11faa3387cada1bdb514f01a994
tmeunier <theo.meunier41@gmail.com>
2024-12-09 11:19:31 +0100

    fix: update password default user

--- src/main/kotlin/fr/tmeunier/domaine/seeder/UserSeeder.kt
+++ src/main/kotlin/fr/tmeunier/domaine/seeder/UserSeeder.kt
@@ -7,7 +7,7 @@ import fr.tmeunier.domaine.repositories.UsersPermissionsRepository
 object UserSeeder {
     suspend fun seed() {
         if (UserRepository.findByEmail("admin@filesox.fr") == null) {
-            val userId = UserRepository.create(null, "adminadmin", "admin@filesox.fr", "filesox", null)
+            val userId = UserRepository.create(null, "Administrator", "admin@filesox.fr", "adminadmin", null)
             val adminRole = PermissionRepository.findName("Administration")
 
             UsersPermissionsRepository.sync(userId, listOf(adminRole.id))
