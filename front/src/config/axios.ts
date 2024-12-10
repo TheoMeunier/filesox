159aafde6f71fe62093114bc0aafb946fce96dbe
tmeunier <theo.meunier41@gmail.com>
2024-12-10 10:40:22 +0100

    fix: docker build for image front

--- front/src/config/axios.ts
+++ front/src/config/axios.ts
@@ -7,6 +7,7 @@ export enum AuthEnum {
 }
 
 export const BASE_URL = import.meta.env.VITE_API_URL;
+console.log(BASE_URL)
 
 export const useAxios = () => {
     const {setAllTokens, logout} = useAuth()
