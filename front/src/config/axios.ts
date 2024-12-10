a0de7537d746a13c0ed67ebce7874e2b61860ee9
tmeunier <theo.meunier41@gmail.com>
2024-12-10 11:00:41 +0100

    fix: code clear

--- front/src/config/axios.ts
+++ front/src/config/axios.ts
@@ -7,7 +7,6 @@ export enum AuthEnum {
 }
 
 export const BASE_URL = import.meta.env.VITE_API_URL;
-console.log(BASE_URL)
 
 export const useAxios = () => {
     const {setAllTokens, logout} = useAuth()
