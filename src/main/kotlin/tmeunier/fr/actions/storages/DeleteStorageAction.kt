28dd4f967e08ee231ff7115ce8b1b5cebc097c3c
tmeunier <theo.meunier41@gmail.com>
2025-08-15 11:34:01 +0200

    fix(storage): delete all recursive folder

--- src/main/kotlin/tmeunier/fr/actions/storages/DeleteStorageAction.kt
+++ src/main/kotlin/tmeunier/fr/actions/storages/DeleteStorageAction.kt
@@ -5,21 +5,34 @@ import tmeunier.fr.databases.entities.FileEntity
 import tmeunier.fr.databases.entities.FolderEntity
 import tmeunier.fr.dtos.requests.DeleteStorageRequest
 import tmeunier.fr.exceptions.storage.StorageNotFoundException
+import tmeunier.fr.services.logger
 import java.time.LocalDateTime
+import java.util.UUID
 
 @ApplicationScoped
 class DeleteStorageAction {
     fun execute(request: DeleteStorageRequest) {
-        (if (request.isFolder) {
-            val folder = FolderEntity.findById(request.id) ?: throw StorageNotFoundException("Folder ${request.id} not found")
-            FileEntity.update("deletedAt = ?1 where path = ?2", LocalDateTime.now(), folder.path + "%", )
-
-            folder.delete()
+        if (request.isFolder) {
+            deleteFolder(request.id)
         } else {
             val file = FileEntity.findById(request.id) ?: throw StorageNotFoundException("File ${request.id} not found")
 
             file.deletedAt = LocalDateTime.now()
             file.persist()
-        })
+        }
+    }
+
+    private fun deleteFolder(folderId: UUID) {
+        val folder = FolderEntity.findById(folderId) ?: throw StorageNotFoundException("Folder ${folderId} not found")
+
+        FileEntity.update("deletedAt = ?1 where parent.id = ?2", LocalDateTime.now(), folder.id)
+
+
+        val subFolders = FolderEntity.list("path LIKE ?1", folder.path + "%")
+        subFolders.forEach {
+            FileEntity.update("deletedAt = ?1 where parent.id = ?2", LocalDateTime.now(), it.id)
+        }
+
+        folder.delete()
     }
 }
