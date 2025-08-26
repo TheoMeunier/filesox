eb76cf9c9667ae4a1888d04455f8f729a773decb
tmeunier <theo.meunier41@gmail.com>
2025-08-26 21:17:06 +0200

    delete compilation cross platform

--- src/main/kotlin/tmeunier/fr/services/PasswordService.kt
+++ src/main/kotlin/tmeunier/fr/services/PasswordService.kt
@@ -1,33 +1,17 @@
 package tmeunier.fr.services
 
-import de.mkammerer.argon2.Argon2
-import de.mkammerer.argon2.Argon2Factory
+import at.favre.lib.crypto.bcrypt.BCrypt
 import jakarta.enterprise.context.ApplicationScoped
-import jakarta.resource.spi.ConfigProperty
-
 
 @ApplicationScoped
-class PasswordService
-{
-    private val argon2: Argon2? = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id)
-
-    var iterations: Int = 2
-    var memory: Int = 65536
-    var parallelism: Int = 1
+class PasswordService {
 
     fun hashPassword(password: String): String {
-        try {
-            return argon2!!.hash(iterations, memory, parallelism, password.toCharArray())
-        } finally {
-            argon2!!.wipeArray(password.toCharArray())
-        }
+        return BCrypt.withDefaults().hashToString(12, password.toCharArray())
     }
 
     fun verifyPassword(password: String, hash: String?): Boolean {
-        try {
-            return argon2!!.verify(hash, password.toCharArray())
-        } finally {
-            argon2!!.wipeArray(password.toCharArray())
-        }
+        val result = BCrypt.verifyer().verify(password.toCharArray(), hash)
+        return result.verified
     }
 }
