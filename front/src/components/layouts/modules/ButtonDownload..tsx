2c92d8da672d29c324198dd60a2b6961a5080c74
tmeunier <theo.meunier41@gmail.com>
2025-08-31 23:08:33 +0200

    fix: download file with permissions

--- front/src/components/layouts/modules/ButtonDownload..tsx
+++ front/src/components/layouts/modules/ButtonDownload..tsx
@@ -3,12 +3,13 @@ import { ButtonIcon } from '../../modules/Button.tsx';
 import { useStorage } from '@hooks/useStorage.ts';
 
 export function ButtonDownload() {
+  const token = localStorage.getItem('token');
   const { isFolder, getPathOrName } = useStorage();
 
   const downloadInSameWindow = () => {
     const url = isFolder()
-      ? `/api/download${getPathOrName()}`
-      : `/api/download/${getPathOrName()}`;
+      ? `/api/download${getPathOrName()}?token=${token}`
+      : `/api/download/${getPathOrName()}?token=${token}`;
     window.location.href = url;
   };
 
