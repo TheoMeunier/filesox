75b7550c12e87bc165d26b5419b2b3fa66c93eae
Théo Meunier <62860058+TheoMeunier@users.noreply.github.com>
2025-09-01 21:18:45 +0200

    fix: delete posibility register (#17)

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './view/auth/Login.tsx';
-import Register from './view/auth/Register.tsx';
 import { App } from './view/layouts/App.tsx';
 import { Dashboard } from './view/Dashboard.tsx';
 import { Profile } from './view/profile/Profile.tsx';
@@ -57,7 +56,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
               </Route>
 
               <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
             </Routes>
           </BrowserRouter>
         </AuthProvider>
